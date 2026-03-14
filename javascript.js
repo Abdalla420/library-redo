896ee8d83253c9e660377b89c4ef2a14a7268db6
Abdalla420 <215827221+Abdalla420@users.noreply.github.com>
2026-03-13 22:12:46 -0400

    Add displayBooks function where it goes through all the books created in the array and create a dom mainpulation for each parameter

--- javascript.js
+++ javascript.js
@@ -12,6 +12,36 @@ function addBookToLibrary(a, t, p, s) {
     const createBook = new Book(a, t, p, s);
     myLibrary.push(createBook);
 }
+function displayBooks() {
+    const shelf = document.querySelector(".shelf-container")
+    for (const value of myLibrary){
+        const card = document.createElement("div");
+        card.className = "card";
 
+        const bookTitle = document.createElement("p");
+        bookTitle.className = "title";
+        bookTitle.textContent = value.title;
+
+        const authorName = document.createElement("p")
+        authorName.className = "author";
+        authorName.textContent = `By ${value.author}`;
+
+        const bookPages = document.createElement("p");
+        bookPages.className = "pages";
+        bookPages.textContent = `${value.pages} pages`
+
+        const bookStatus = document.createElement("p");
+        bookStatus.className = "status";
+        if (value.status == true){
+            bookPages.textContent = `✅ Read`;
+        }else {
+            bookPages.textContent = "📚 Unread";
+        }
+
+        card.append(bookTitle, authorName, bookPages, bookStatus);
+        shelf.append(card);
+    }
+}
 addBookToLibrary("Mo", "egypt", 69, true);
 addBookToLibrary("not-Mo", "not-egypt", 67, false);
+displayBooks();
